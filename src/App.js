3d74e818840aef851df8d32e2d8c61a043b46962
DimaVax <126974388+DimaVax@users.noreply.github.com>
2025-09-13 12:24:01 +0200

    Дз 2-3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import { Header } from "./Components/Header/Header";
 import { Home } from "./Components/Home/Home";
 import { Movies } from "./Components/Movies/Movies";
+import { Movie } from "./Components/Movie/Movie";
+import { Cast } from "./Components/Movie/Cast/Cast";
+import { Rewiews } from "./Components/Movie/Rewiews/Rewiews";
 import "./App.css";
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
+        <Route path="/movie/:id" element={<Movie />} />
+        <Route path="/movie/:id/rewiews" element={<Rewiews />} />
+        <Route path="/movie/:id/cast" element={<Cast />} />
       </Routes>
     </div>
   );
